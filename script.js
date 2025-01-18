25ca78a0dde402732898038fc0f69e1d1f41b8dc
Martynas0 <siupinysm@gmail.com>
2025-01-18 12:23:07 +0000

    Add ability to resize canvas grid

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container = document.querySelector(".main-container");
+const inputForm = document.querySelector(".input");
 
-createGrid(200);
+createGrid(100);
 
 container.addEventListener("mouseover", (event) => {
     const element = event.target;
@@ -10,6 +11,19 @@ container.addEventListener("mouseover", (event) => {
     }
 })
 
+inputForm.addEventListener("click", (event) => {
+    let inputValue = Number(document.querySelector(".input input").value);
+    if (event.target.id === "change-size-button") {
+        if (inputValue !== "NaN" && inputValue > 0 && inputValue <= 200) {
+            removeGrid();
+            createGrid(inputValue);
+        }
+        else {
+            alert("You must enter a number from 1 to 200 !");
+        }
+    }    
+})
+
 function createGrid(size) {
     for (let i = 0; i < size; i++) {
         const row = createRow();
@@ -17,7 +31,10 @@ function createGrid(size) {
     }
 }
 
-
+function removeGrid () {
+    const listOfRows = document.querySelectorAll(".row");
+    listOfRows.forEach( element => element.remove() );
+}
 
 
 
